fe10f47f48cb345bb0e315457bedb406115d78d3
Seok <tjr50999@naver.com>
2021-02-27 22:05:22 +0900

    Add Word-Relay

---
+++ JS/Word-Relay/main.js
@@ -0,0 +1,17 @@
+let number = Number(prompt("몇 명이 참가하나요?"));
+const $button = document.querySelector('button');
+const $input = document.querySelector('input');
+let word;     // 제시어
+
+
+const onClickButton = () => {
+
+};
+
+const onInput = (e) => {
+  
+};
+
+
+$button.addEventListener('click', onClickButton);
+$input.addEventListener('input', onInput);
