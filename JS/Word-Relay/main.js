8fff7f612c8eb3eb17f6273a1dfe872fdc54d786
sharpcoder312 <tjr50999@naver.com>
2021-04-03 15:45:12 +0900

    Upgrade Word-Relay 1st

--- JS/Word-Relay/main.js
+++ JS/Word-Relay/main.js
@@ -1,33 +1,52 @@
-let number = Number(prompt("몇 명이 참가하나요?"));
-const $button = document.querySelector('button');
-const $input = document.querySelector('input');
-const $word = document.querySelector('.word');
-const $order = document.querySelector('.order');
-let word;     // 제시어
+let number = Number(prompt("참가 인원을 말해주세요"));
+const $button = document.querySelector("button");
+const $input = document.querySelector("input");
+const $word = document.querySelector(".word");
+const $order = document.querySelector(".order");
+let word;   // 제시어 
 let newWord;    // 현재 단어
 
-
-const onClickButton = () => { 
+const onClick = () => {
   if (!word || word[word.length - 1] === newWord[0]) {  // 제시어가 비거나 올바른 단어인가?
-    word = newWord;                  // 입력한 단어가 제시어가 된다
-    $word.textContent = word;        // 화면에 제시어 표시
-    const order = Number($order.textContent);
-    if (order + 1 > number) {
-      $order.textContent = 1;
+    word = newWord;                                     // 입력한 단어가 제시어가 된다
+    $word.innerText = word;                             // 화면에 제시어 표시
+    if (Number($order.innerText) + 1 > number) {
+      $order.innerText = 1;
     } else {
-      $order.textContent = order + 1;
+      $order.innerText = Number($order.innerText) + 1
     }
-  } else {  // 올바르지 않다.
-    alert('올바르지 않은 단어입니다!');
+  } else {
+    alert('틀렸다능'); 
   }
-      $input.value = '';
-      $input.focus();
-};
+  $input.value = ""
+  $input.focus();
+}
 
 const onInput = (e) => {
   newWord = e.target.value; // 입력한 단어를 현재 단어로
-};
+}
 
+const onEnter = (e) => {
+  newWord = e.target.value; // 입력한 단어를 현재 단어로
+  if (!word || word[word.length - 1] === newWord[0]) {
+    word = newWord;
+    $word.innerText = word;
+    if (Number($order.innerText) + 1 > number) {
+      $order.innerText = 1;
+    } else {
+      $order.innerText = Number($order.innerText) + 1
+    }
+    $input.value = ""
+    $input.focus();
+  } else {
+    alert('You lost');
+    setTimeout(() => {
+      window.location.reload();
+    }, 1000);
+  }
+}
 
-$button.addEventListener('click', onClickButton);
+$button.addEventListener('click', onClick);
 $input.addEventListener('input', onInput);
+$input.addEventListener('keypress', onEnter);
+
