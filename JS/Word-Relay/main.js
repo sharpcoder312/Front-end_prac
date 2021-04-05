1d4b7e0ee8637d0c250ca2cfdbf06f20bc7fb9ca
sharpcoder312 <tjr50999@naver.com>
2021-04-06 01:11:29 +0900

    Upgrade Word-Relay 2nd

--- JS/Word-Relay/main.js
+++ JS/Word-Relay/main.js
@@ -9,11 +9,11 @@ let newWord;    // 현재 단어
 const onClick = () => {
   if (!word || word[word.length - 1] === newWord[0]) {  // 제시어가 비거나 올바른 단어인가?
     word = newWord;                                     // 입력한 단어가 제시어가 된다
-    $word.innerText = word;                             // 화면에 제시어 표시
-    if (Number($order.innerText) + 1 > number) {
-      $order.innerText = 1;
+    $word.textContent = word;                             // 화면에 제시어 표시
+    if (Number($order.textContent) + 1 > number) {
+      $order.textContent = 1;
     } else {
-      $order.innerText = Number($order.innerText) + 1
+      $order.textContent = Number($order.textContent) + 1
     }
   } else {
     alert('틀렸다능'); 
@@ -30,11 +30,11 @@ const onEnter = (e) => {
   newWord = e.target.value; // 입력한 단어를 현재 단어로
   if (!word || word[word.length - 1] === newWord[0]) {
     word = newWord;
-    $word.innerText = word;
-    if (Number($order.innerText) + 1 > number) {
-      $order.innerText = 1;
+    $word.textContent = word;
+    if (Number($order.textContent) + 1 > number) {
+      $order.textContent = 1;
     } else {
-      $order.innerText = Number($order.innerText) + 1
+      $order.textContent = Number($order.textContent) + 1
     }
     $input.value = ""
     $input.focus();
@@ -49,4 +49,3 @@ const onEnter = (e) => {
 $button.addEventListener('click', onClick);
 $input.addEventListener('input', onInput);
 $input.addEventListener('keypress', onEnter);
-
