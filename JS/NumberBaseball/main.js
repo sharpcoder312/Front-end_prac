736e24eea22c5a51af97955d682eee98f01bced5
Seok <tjr50999@naver.com>
2021-05-02 21:55:26 +0900

    Update NB [comment]

--- JS/NumberBaseball/main.js
+++ JS/NumberBaseball/main.js
@@ -1,5 +1,5 @@
 const $input = document.querySelector('#input');
-const $form = document.querySelector('#form');
+const $form = document.querySelector('#form');  // form을 쓰는 경우에는 보통 input이 아닌 form에 eventlistener를 달아준다. form내의 다른 태그들을 가리키고 싶을 땐 event객체를 사용하자. ex) event.target[0] = input // event.target[1] = submit
 const $logs = document.querySelector('#logs');
 
 const numbers = [];
@@ -17,6 +17,8 @@ for (let n = 0; n < 9; n += 1) {
 // console.log(answer);
 // 이 코드의 문제는 (Math.random() * 9)를 사용하게 됐을 때, 간혹 숫자 대신 undefined가 뽑혀 나올 때가 있다는 것이다.
 // 함수가 한번 돌고나면 요소가 하나 줄어들 것인데, 하필 다음 함수가 돌때 남은 요소 개수보다 큰 index값이 무작위에서 뽑혀나온다면 undefined가 나오는 원리이다.
+// 이렇게 예외를 발견하는 방법은 극단적인 수나 맨 처음 값, 맨 끝 값을 생각해보면 쉽게 생각할 수 있다.
+
 
 const answer = [];
 for (let n = 0; n < 4; n++) {     // 4번 반복
@@ -25,6 +27,7 @@ for (let n = 0; n < 4; n++) {     // 4번 반복
   numbers.splice(index, 1);
 }
 console.log(answer);
+
 
 const tries = [];
 function checkInput(input) {
@@ -39,10 +42,14 @@ function checkInput(input) {
   }
   return true;                    // 검사 통과 - true return // 검사 실패 - false return
 }                                 // alert 함수는 기본적으로 undefined를 return한다. if문에서는 undefined를 flase 처리하므로 결과는 같다.
+// 사실 위의 코드는 HTML5의 검증 기능으로도 대체 가능하다. ex) required, minlength, maxlength, pattern ...
+// 하지만 error 메세지의 디자인 변경이 어려우며 최근 잘 쓰지않는 추세이다.
+
+
 $form.addEventListener('submit', (event) => {
   event.preventDefault();
   const value = $input.value;
-  $input.value = '';
+  $input.value = '';              // === event.target[0].value = '';
   const valid = checkInput(value);
   if (!valid) return;
   if (answer.join('') === value) {
@@ -54,6 +61,8 @@ $form.addEventListener('submit', (event) => {
     $logs.appendChild(message);       // createTextNode와 appendChild를 쓴 이유는 textContent를 쓰면 기존 내용이 사라지기 때문이다.
     return;
   }
+
+
   // 몇 스트라이크 몇 볼인지 검사
   let strike = 0;
   let ball = 0;
