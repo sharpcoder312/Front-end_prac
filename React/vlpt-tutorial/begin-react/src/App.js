2f06398920da5751ecee5f7279f2c7af12d8d5d5
Bebop <tjr50999@naver.com>
2021-09-03 22:22:19 +0900

    Add: [08 input 상태 관리하기]

--- React/vlpt-tutorial/begin-react/src/App.js
+++ React/vlpt-tutorial/begin-react/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';  
 import Counter from './Counter';
+import InputSample from './inputSample';
 // 사실 import는 함수에 불러올 component를 입력하면서 자동 완성 기능을 통해 불러올 수도 있다.
 
 function App() {
     return (
+      <>
       <Counter />
+      <InputSample />
+      </>
     )
 }
 
---
+++ React/vlpt-tutorial/begin-react/src/inputSample.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+function InputSample() {
+  const [content, setContent] = useState('');
+
+  const changeContent = (e) => {
+    setContent(e.target.value)
+  }
+
+  const resetContent = (e) => {
+    // setContent(e.target.value = ''); 이렇게 쓸 필요 굳이 없음.
+    setContent('');
+  }
+
+  return (
+    <div>
+      <input onChange={changeContent} value={content} />  {/* input의 초기값(value)로 content를 가져오는 것이 중요하다. */}
+      <button onClick={resetContent}>초기화</button>
+      <div>
+        <b>값: </b>
+        {content}
+      </div>
+    </div>
+  )
+}
+
+export default InputSample
