c3ec384ccc198918b759338e69c0771c922dbb17
Bebop <tjr50999@naver.com>
2021-09-05 17:44:28 +0900

    Add: [12 useRef로 컴포넌트 안의 변수 만들기]

--- React/vlpt-tutorial/begin-react/src/App.js
+++ React/vlpt-tutorial/begin-react/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';  
+import React, { useRef } from 'react';  
 import Counter from './Counter';
 import InputSample from './inputSample';
 import InputSample2 from './inputSample2';
@@ -7,12 +7,37 @@ import UserList from './UserList';
 // 사실 import는 함수에 불러올 component를 입력하면서 자동 완성 기능을 통해 불러올 수도 있다.
 
 function App() {
+    const users = [
+      {
+        id: 1,
+        username: 'seok',
+        email: 'seok.@example.com'
+      },
+      {
+        id: 2,
+        username: 'seokk',
+        email: 'seokk.@example.com'
+      },
+      {
+        id: 3,
+        username: 'seokkk',
+        email: 'seokkk.@example.com'
+      },
+    ];
+
+    const nextId = useRef(4); // 여기서 nextd를 useRef로 관리해준 이유는 4라는 값이 바뀐다고해서 굳이 component가 리렌더링 될 필요가 없기 때문이다.
+
+    const onCreate = () => {
+      console.log(nextId.current); // 4
+      nextId.current += 1;
+    }
+
     return (
       <>
       <Counter />
       <InputSample />
       <InputSample2 />
-      <UserList />
+      <UserList users={users}/>
       </>
     )
 }
--- React/vlpt-tutorial/begin-react/src/UserList.js
+++ React/vlpt-tutorial/begin-react/src/UserList.js
@@ -8,29 +8,7 @@ function User({user}) {
   )
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'seok',
-      email: 'seok.@example.com'
-    },
-    {
-      id: 2,
-      username: 'seokk',
-      email: 'seokk.@example.com'
-    },
-    {
-      id: 3,
-      username: 'seokkk',
-      email: 'seokkk.@example.com'
-    },
-    {
-      id: 4,
-      username: 'seokkkk',
-      email: 'seokkkk.@example.com'
-    },
-  ];
+function UserList({ users }) {
 
   // return ( // ( 원시적 방법 )
   //   <div>
@@ -47,27 +25,27 @@ function UserList() {
   //   </div>
   // )
 
-  return ( // 배열의 수가 고정적일때
-    <div>
-      <User user={users[0]}/>
-      <User user={users[1]}/>
-      <User user={users[2]}/>
-    </div>
-  )
-
-  // return ( // 배열의 수가 고정적이지 않고 내용이 변경될 가능성이 있을 때 - map함수 사용. 객체 배열 형태로 있는 배열을 컴포넌트 element형태의 배열로 변환
+  // return ( // 배열의 수가 고정적일때
   //   <div>
-  //     <h5>11 배열의 렌더링</h5>
-  //     {
-  //       users.map(
-  //         user => (<User user={user} key={user.id} />) // 각 child(현재는 User)는 key props이 있어야함. key props은 각 원소들 마다 고유값을 줌으로써 리렌더링 성능을 최적화해줌.
-  //         )
-  //       // users.map (
-  //       //   (user, index) => (<User user={user} key={index} />) // key로 사용할 고유값이 없을 때는 map 콜백함수의 두번째 parm인 index값을 사용하자. 하지만 이렇게한다고해서 성능이 좋아지는 것은 아니니 index를 넣어주는 것을 될수있으면 피하도록하자.
-  //       // ) // key가 없으면 각 배열의 원소가 정확히 어떤 것을 렌더링 해야하는지 잘 모른다. 단순히 자신의 index값만 기억할 뿐이다.
-  //     }
+  //     <User user={users[0]}/>
+  //     <User user={users[1]}/>
+  //     <User user={users[2]}/>
   //   </div>
   // )
+
+  return ( // 배열의 수가 고정적이지 않고 내용이 변경될 가능성이 있을 때 - map함수 사용. 객체 배열 형태로 있는 배열을 컴포넌트 element형태의 배열로 변환
+    <div>
+      <h5>11 배열의 렌더링</h5>
+      {
+        users.map(
+          user => (<User user={user} key={user.id} />) // 각 child(현재는 User)는 key props이 있어야함. key props은 각 원소들 마다 고유값을 줌으로써 리렌더링 성능을 최적화해줌.
+          )
+        // users.map (
+        //   (user, index) => (<User user={user} key={index} />) // key로 사용할 고유값이 없을 때는 map 콜백함수의 두번째 parm인 index값을 사용하자. 하지만 이렇게한다고해서 성능이 좋아지는 것은 아니니 index를 넣어주는 것을 될수있으면 피하도록하자.
+        // ) // key가 없으면 각 배열의 원소가 정확히 어떤 것을 렌더링 해야하는지 잘 모른다. 단순히 자신의 index값만 기억할 뿐이다.
+      }
+    </div>
+  )
 }
 
 export default UserList;
