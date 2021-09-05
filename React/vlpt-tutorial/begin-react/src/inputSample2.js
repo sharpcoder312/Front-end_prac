6c621ec185cfca6125877e952030fc422e177f8f
Bebop <tjr50999@naver.com>
2021-09-05 15:40:41 +0900

    Add: [10 useRef로 특정 DOM 선택하기]

--- React/vlpt-tutorial/begin-react/src/inputSample2.js
+++ React/vlpt-tutorial/begin-react/src/inputSample2.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useRef} from 'react';
 // useState에서 기존에는 단지 문자열 값을 관리했지만 '문자', '숫자' 뿐만아니라 여러 개의 문자열을 저장하고 있는 '배열'이나 '객체'도 저장 가능하다.
 
 function InputSample() {
@@ -7,7 +7,7 @@ function InputSample() {
     nickname: '',
   });
   // 여러 개의 문자열을 가지는 객체 형태의 상태 관리
-
+  const nameInput = useRef();
   const { name, nickname } = inputs;
   // 객체 비구조 할당
 
@@ -36,12 +36,14 @@ function InputSample() {
       name: '',
       nickname: ''
     })
+    nameInput.current.focus(); // current는 해당 특정 DOM을 가리키는 역할을 한다.
   }
 
   return (
     <div>
       <h5>09 여러 개의input 상태 관리하기</h5> {/*  input에 name값을 설정하고 event가 발생했을 때 이 값을 참조 */}
-      <input name="name" placeholder="이름" onChange={changeContent} value={name} />
+      <h5>10 useRef로 특정 DOM 선택하기</h5>
+      <input name="name" placeholder="이름" onChange={changeContent} value={name} ref={nameInput}/>
       <input name="nickname" placeholder="닉네임" onChange={changeContent} value={nickname} />
       <button onClick={resetContent}>초기화</button>
       <div>
