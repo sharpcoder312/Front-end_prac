dd49d7b3ecc0134d33cf42f5cca75c2aab725e9b
Bebop <tjr50999@naver.com>
2021-09-08 18:27:04 +0900

    Add : [16 마운트, 언마운트, 업데이트 시 useEffect 사용하여 작업하기]

--- React/vlpt-tutorial/begin-react/src/UserList.js
+++ React/vlpt-tutorial/begin-react/src/UserList.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
   const { username, email, id, active } = user;
+  useEffect(() => {
+    console.log('컴포넌트가 화면에 나타남');
+  }, [])
   return (
       <div>
         <b style={{
