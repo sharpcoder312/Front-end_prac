09d6bbecdc8c5248e021a54be984d7ae7c49d666
Bebop <tjr50999@naver.com>
2021-09-16 10:52:01 +0900

    Style: [Add TodoCreate Component UI 2nd]

--- React/basic-todolist/src/components/TodoCreate.js
+++ React/basic-todolist/src/components/TodoCreate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { css } from 'styled-components' // 조건부로 스타일 지정하기위해 { css }를 import한다.
 import { MdAdd } from 'react-icons/md'
 
@@ -30,14 +30,62 @@ const CircleButton = styled.button`
 
   border: none;
   outline: none;
+
+  transition: 0.125s all ease-in;
+  ${props => props.open && css`
+    background: #ff6b6b;
+    &:hover {
+      background: #ff8787;
+    }
+    &:active {
+      background: #fa5252;
+    }
+    transform: translate(-50%, 50%) rotate(45deg);
+  `}
 `;
 
+const InsertFormPositioner = styled.div`
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: absolute;
+`
+
+const InsertForm = styled.div`
+  background: #f8f9fa;
+  padding: 32px;
+  padding-bottom: 72px;
+  border-bottom-left-radius: 16px;
+  border-bottom-right-radius: 16px;
+  border-top: 1px solid #e9ecef;
+`
+
+const Input = styled.input`
+  padding: 12px;
+  border-radius: 4px;
+  border: 1px solid #dee2e6;
+  width: 100%;
+  outline: none;
+  font-size: 18px;
+  box-sizing: border-box;
+`
+
 function TodoCreate() {
+  const [open, setOpen] = useState(false);
+  const onToggle = () => setOpen(!open);
+
   return (
-    <CircleButton>
+    <>
+    {open &&
+      <InsertFormPositioner>
+        <InsertForm>
+          <Input placeholder="할 일을 입력 후, Enter를 누르세요" autoFocus />  {/* autoFocus로 컴포넌트 렌더링시 자동으로 포커스가 잡히게끔 한다. */}
+        </InsertForm>
+      </InsertFormPositioner>}
+    <CircleButton onClick={onToggle} open={open}>
       <MdAdd />
     </CircleButton>
-
+    </>
   );
 }
 
