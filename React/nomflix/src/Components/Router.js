aac820674f048ba75aaa0a1dc7fbb3e82ac8adc4
Bebop <tjr50999@naver.com>
2021-09-22 22:15:31 +0900

    Update: [BrowserRouter 추가]

--- React/nomflix/src/Components/Router.js
+++ React/nomflix/src/Components/Router.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
 
 const routes = () => (
   <Router>
-    <>
+    <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
+      <Route path="/tv/popular" exact render={() => <h1>Popular</h1>} />
       <Route path="/search" exact component={Search} />
-    </>
-  </Router>)
+      <Redirect from="*" to="/" />
+      {/* Redirect의 역할은 해당 Router에 일치하는 Route가 없다면 어느 페이지든 받아서 '/'로 보내준다. */}
+    </Switch>
+  </Router>
+  )
 
 export default routes
 
