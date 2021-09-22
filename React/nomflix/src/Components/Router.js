6000d775bcbc7c458114e44d2116d00e37f8b511
Bebop <tjr50999@naver.com>
2021-09-22 23:36:01 +0900

    Update: [styled-component 세팅]

--- React/nomflix/src/Components/Router.js
+++ React/nomflix/src/Components/Router.js
@@ -2,18 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
+import Header from "../Routes/Header";
 import Search from "../Routes/Search";
 
 const routes = () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Route path="/tv" exact component={TV} />
-      <Route path="/tv/popular" exact render={() => <h1>Popular</h1>} />
-      <Route path="/search" exact component={Search} />
-      <Redirect from="*" to="/" />
+    <>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/tv" component={TV} />
+        <Route path="/search" component={Search} />
+        <Redirect from="*" to="/" />
       {/* Redirect의 역할은 해당 Router에 일치하는 Route가 없다면 어느 페이지든 받아서 '/'로 보내준다. */}
-    </Switch>
+      </Switch>
+    </>
   </Router>
   )
 
