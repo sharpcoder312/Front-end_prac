275e471a4efbd36a33d299ad29209db882cbe6f0
Bebop <tjr50999@naver.com>
2021-10-12 21:00:11 +0900

    Feat: [conbineReducers를 이용하여 reducer 합치기]

--- React/shop/src/Cart.js
+++ React/shop/src/Cart.js
@@ -26,14 +26,26 @@ function Cart(props) {
           })
         }
       </Table>
+      { props.alert === true
+      ? (
+        <div className="my-alert">
+        <p>지금 구매하시면 신규 할인 20%</p>
+        <button onClick={()=>{props.dispatch({ type: '끄기' })}}>닫기</button>
+      </div>
+      )
+      : null
+      }
     </div>
   )
 }
 
 function Store(state){ // redux store 데이터를 가져와서 props로 만들어주는 함수
+  console.log(state)
   return {
     // 상품명 : state[0].name, // state의 name이라는 데이터를 상품명이라는 props로 바꿔라
-    state: state // state의 모든 데이터를 state라는 이름의 props로 바꿔라.
+    // state: state. // state의 모든 데이터를 state라는 이름의 props로 바꿔라.
+    state: state.reducer, // reducer가 다수이기에 state.로 특정 reducer를 명시해준다. 
+    alert: state.reducer2
   }
 }
 
--- React/shop/src/index.js
+++ React/shop/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 
 let defaultState = [ // 초기값. 즉, 변경 x값
   { id : 0, name : 'niceshoe', quan : 2},
@@ -29,7 +29,20 @@ function reducer(state = defaultState, action) {
 } // state = defaultState에서 =로 state에 할당해준 것은 기본 값을 지정해주는 default parameter ES6 신 문법이다. 여기서는 state의 기본값을 설정해준 것이다.
   // reducer는 그것이 수정되든 수정되지않든 항상 특정 state를 반환해야한다. 
 
-let store = createStore(reducer);
+function reducer2(state = true, action) {
+  if (action.type === '끄기') {
+    state = false;
+    return state;
+    // Array, Object state가 아니기에 굳이 복사할 필요가 없다.
+  } else {
+    return state
+  }
+} // 사실 해당 기능은 cart에서만 쓰기 때문에 굳이 index의 redux에 저장할 필요가 없다.
+// 즉, redux store에 온갖 데이터를 저장하지 말고 특정 소수의 component에서만 쓰는 것들은 useState()에 저장해서 사용하자.
+
+let store = createStore(combineReducers({reducer, reducer2}));
+// reducer함수가 다수라면 combineReducer를 이용하여 createStore인자에 넣어 합쳐줘야한다.
+// 유의해야 할 점은 reducer 다수가 합쳐지면 컴포넌트에서 data를 뽑아쓸 때도 유의해야한다.
 
 ReactDOM.render(
   <React.StrictMode>
